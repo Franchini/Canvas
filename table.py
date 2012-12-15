c39e709f2db88cf434cedbb3005f6baf1e41771a
Mirko Mälicke <mirko@maelicke-online.de>
2012-12-15 15:05:55 +0100

    0.21
    TableWindow includes now statistical area performing some native
    statistical analysis at this point

--- table.py
+++ table.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os,  math
 from PyQt4 import QtGui, QtCore
 from ui.Ui_table import Ui_TableWindow as table
 
@@ -17,6 +17,9 @@ class TableWindow (QtGui.QDialog, table):
             self.__setup(self.Data)
             self.__showData(self.Data.getAllData())
             
+        # set slots
+        self.connect(self.checkTool, QtCore.SIGNAL("stateChanged(int)"),  self.__oncheckTool)
+        self.connect(self.tableWidget, QtCore.SIGNAL("itemSelectionChanged()"), self.__onSelectionChanged)
         
     def __showData(self, data):
         #includes given data into the QTableWidget
@@ -37,5 +40,90 @@ class TableWindow (QtGui.QDialog, table):
         for i, depth in enumerate(depths):
             item = QtGui.QTableWidgetItem()
             item.setText(str(depth))
-            self.tableWidget.setHorizontalHeaderItem(i, item)
+            self.tableWidget.setHorizontalHeaderItem(i,  item)
             
+    def __oncheckTool(self):
+        # check status of checkBox and show or hide tabwidget by setting window size
+        if self.checkTool.checkState() == 2:
+            #show tabwidget by resizing TableWindow
+            self.setMinimumSize(QtCore.QSize(641, 665))
+            self.setMaximumSize(QtCore.QSize(641, 665))
+            self.resize(641, 665)
+        elif self.checkTool.checkState() == 0:
+            #hide tabwidget by resizing TableWindow
+            self.setMinimumSize(QtCore.QSize(641, 416))
+            self.setMaximumSize(QtCore.QSize(641, 416))
+            self.resize(641, 416)
+        
+    def __onSelectionChanged(self):
+        # calculate new statistical output for selected data
+        #print "got Selection %s"%str(self.tableWidget.selectedItems())
+        self.selection = Statistic(self.tableWidget.selectedItems())
+        
+        if len(self.tableWidget.selectedItems()) == 0:
+            # nothing selected
+            self.statisticOutput.setText("This is the statistical output area.\nSelect data to list its statistical properties")
+        else:
+            # print the output
+            self.statisticOutput.setText("Statistic Output\n\n")
+            self.statisticOutput.setText(self.statisticOutput.text() + "Mean Value:\t\t" + str(self.selection.getMean()) +"\n")
+            self.statisticOutput.setText(self.statisticOutput.text() + "Maximum Value:\t\t" + str(self.selection.getMax()) +"\n")
+            self.statisticOutput.setText(self.statisticOutput.text() + "Minimum Value:\t\t" + str(self.selection.getMin()) +"\n")
+            self.statisticOutput.setText(self.statisticOutput.text() + "Sample Variance:\t\t" + str(self.selection.getVar()) +"\n")
+        
+
+class Statistic (object):
+    def __init__(self, QTableWidgetItems):
+        # create a list of all QTableWidgetItem values
+        self.__data = []
+        for item in QTableWidgetItems:
+            self.__data.append(float(item.text()))
+        
+    def getMean(self):
+        # returns the mean value of of given data list
+        mean = 0;
+        for i, value in enumerate(self.__data):
+            mean += value
+        mean /= (i+1)                                   # as i starts with 0
+        return mean
+        
+    def getMin(self):
+        # returns the minimum value of given data list
+        minimum = float('inf')
+        
+        for value in self.__data:
+            if value < minimum:
+                minimum = value
+        return minimum
+        
+    def getMax(self):
+        # returns the maximum value of given data list
+        maximum = -float('inf')
+        
+        for value in self.__data:
+            if value > maximum:
+                maximum = value
+        return maximum
+        
+    def length(self):
+        #returns the number n of elements
+        return len(self.__data)
+        
+    def getSum(self):
+        # returns the sum of all values in data
+        sum = 0
+        for value in self.__data:
+            sum += value
+        return sum
+        
+    def getVar(self):
+        # returns the variance of given values in data, using sample variance
+        mean = self.getMean()
+        sq = 0
+        for value in self.__data:
+            sq += math.pow((value - mean), 2)
+        try:
+            res = float(sq) * float(1.0/(float(self.length()) - 1.0))
+        except ZeroDivisionError:
+            res = 0             # as n = 1
+        return res
--- ui/Ui_table.py
+++ ui/Ui_table.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'table.ui'
+# Form implementation generated from reading ui file 'D:\Dokumente\Studium\Projects\KSS Projekt\Canvas\ui\table.ui'
 #
-# Created: Wed Jun 27 17:19:47 2012
-#      by: PyQt4 UI code generator 4.9.1
+# Created: Sat Dec 15 12:24:24 2012
+#      by: PyQt4 UI code generator 4.9.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -17,29 +17,61 @@ except AttributeError:
 class Ui_TableWindow(object):
     def setupUi(self, TableWindow):
         TableWindow.setObjectName(_fromUtf8("TableWindow"))
-        TableWindow.resize(612, 437)
+        TableWindow.resize(641, 416)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(TableWindow.sizePolicy().hasHeightForWidth())
+        TableWindow.setSizePolicy(sizePolicy)
+        TableWindow.setMinimumSize(QtCore.QSize(641, 416))
+        TableWindow.setMaximumSize(QtCore.QSize(641, 416))
         self.tableWidget = QtGui.QTableWidget(TableWindow)
-        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 611, 431))
+        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 641, 381))
+        self.tableWidget.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+        self.tableWidget.setSelectionBehavior(QtGui.QAbstractItemView.SelectItems)
         self.tableWidget.setRowCount(13)
         self.tableWidget.setColumnCount(9)
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
-        item = QtGui.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(0, item)
-        item = QtGui.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(1, item)
-        item = QtGui.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(2, item)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.tabTools = QtGui.QTabWidget(TableWindow)
+        self.tabTools.setGeometry(QtCore.QRect(0, 420, 641, 241))
+        self.tabTools.setFocusPolicy(QtCore.Qt.TabFocus)
+        self.tabTools.setObjectName(_fromUtf8("tabTools"))
+        self.tabFilter = QtGui.QWidget()
+        self.tabFilter.setObjectName(_fromUtf8("tabFilter"))
+        self.tabTools.addTab(self.tabFilter, _fromUtf8(""))
+        self.tabStatistic = QtGui.QWidget()
+        self.tabStatistic.setFocusPolicy(QtCore.Qt.TabFocus)
+        self.tabStatistic.setObjectName(_fromUtf8("tabStatistic"))
+        self.statisticOutput = QtGui.QLabel(self.tabStatistic)
+        self.statisticOutput.setGeometry(QtCore.QRect(10, 20, 601, 111))
+        self.statisticOutput.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.statisticOutput.setObjectName(_fromUtf8("statisticOutput"))
+        self.tabTools.addTab(self.tabStatistic, _fromUtf8(""))
+        self.checkTool = QtGui.QCheckBox(TableWindow)
+        self.checkTool.setGeometry(QtCore.QRect(10, 390, 131, 17))
+        self.checkTool.setObjectName(_fromUtf8("checkTool"))
 
         self.retranslateUi(TableWindow)
+        self.tabTools.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(TableWindow)
 
     def retranslateUi(self, TableWindow):
         TableWindow.setWindowTitle(QtGui.QApplication.translate("TableWindow", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.tableWidget.setSortingEnabled(True)
-        item = self.tableWidget.horizontalHeaderItem(0)
-        item.setText(QtGui.QApplication.translate("TableWindow", "X", None, QtGui.QApplication.UnicodeUTF8))
-        item = self.tableWidget.horizontalHeaderItem(1)
-        item.setText(QtGui.QApplication.translate("TableWindow", "Y", None, QtGui.QApplication.UnicodeUTF8))
-        item = self.tableWidget.horizontalHeaderItem(2)
-        item.setText(QtGui.QApplication.translate("TableWindow", "Z", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabTools.setTabText(self.tabTools.indexOf(self.tabFilter), QtGui.QApplication.translate("TableWindow", "Filter", None, QtGui.QApplication.UnicodeUTF8))
+        self.statisticOutput.setText(QtGui.QApplication.translate("TableWindow", "This is the statistical output area.\n"
+"Select data to list its statistical properties", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabTools.setTabText(self.tabTools.indexOf(self.tabStatistic), QtGui.QApplication.translate("TableWindow", "Statistics", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkTool.setText(QtGui.QApplication.translate("TableWindow", "enable Statistic Tools", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    TableWindow = QtGui.QDialog()
+    ui = Ui_TableWindow()
+    ui.setupUi(TableWindow)
+    TableWindow.show()
+    sys.exit(app.exec_())
 
